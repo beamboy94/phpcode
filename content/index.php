0edbcb0aa1e9b37b2636600ddb66cc6c474aa1f8
vkrvino <vkr.vinoth94@GMAIL.COM>
2018-08-13 10:09:33 +0530

    ip

--- content/index.php
+++ content/index.php
@@ -4,9 +4,7 @@
  </head>
  <body>
  <?php echo '<p>This is my PHP demo</p>'; 
- $ipaddress = $_SERVER['REMOTE_ADDR'];
- $_SERVER;
- echo $ipaddress;
+ $_SERVER['REMOTE_ADDR'];
  ?>
  </body>
 </html>
