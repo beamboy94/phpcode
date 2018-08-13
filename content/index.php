3e3cb54aba3aa6a871f5a3b6325dd9cee733a42d
vkrvino <vkr.vinoth94@GMAIL.COM>
2018-08-13 10:04:39 +0530

    HTML file addition

---
+++ content/index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Docker CI/CD Testing</title>
+ </head>
+ <body>
+ <?php echo '<p>This is my PHP demo</p>'; 
+ $ipaddress = $_SERVER['REMOTE_ADDR'];
+ $_SERVER;
+ echo $ipaddress;
+ ?>
+ </body>
+</html>
