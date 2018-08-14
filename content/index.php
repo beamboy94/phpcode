59475f0e0f25109e9e9c472374fc7f9315585d81
vkrvino <vkr.vinoth94@GMAIL.COM>
2018-08-14 09:55:34 +0530

    Image

--- content/index.php
+++ content/index.php
@@ -7,7 +7,7 @@
     <table border="0" cellpadding="10">
       <tr>
         <td>
-          <img src="images/springsource.png">
+          <img src="https://www.google.co.in/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiy-_m52evcAhXBfX0KHXWiA5wQjRx6BAgBEAQ&url=https%3A%2F%2Ftwitter.com%2Femeriocorp&psig=AOvVaw0SdU_qFFzasATBOyhCkElb&ust=1534307086822246">
         </td>
         <td>
           <h1> Initial Workshop Success </h1>
